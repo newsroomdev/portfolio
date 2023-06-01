077d876acb7dfe7aa9d3b04ec35d782879a20950
Gerald Rich <gerald.a.rich@gmail.com>
2023-05-31 21:34:45 -0400

    update gha

--- svelte.config.js
+++ svelte.config.js
@@ -8,10 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter(),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/sveltekit-gh-pages' : ''
-		}
+		adapter: adapter()
 	}
 };
 
