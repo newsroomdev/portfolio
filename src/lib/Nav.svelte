055f449747b6950692a56b577c75a84ad770aa57
Rich, Gerald <grich@ap.org>
2023-06-01 20:26:12 -0400

    add nav

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,33 @@
+<script>
+	import { base } from '$app/paths';
+</script>
+
+<nav>
+	<div id="page-headr" class="animated slide-down headroom--top">
+		<h1 class="col-1-3" id="hed">Gerald Rich</h1>
+		<div class="col-1-3 sub-nav" id="resume">
+			<a target="_blank" rel="noopener" href="{base}/GeraldRichResume.pdf">Resume</a>
+		</div>
+		<div class="col-1-3 sub-nav">
+			<a href="https://www.linkedin.com/in/geraldrich/">LinkedIn</a>
+		</div>
+	</div>
+</nav>
+
+<style>
+	#page-headr {
+		font-family: 'Gill Sans', Helvetica, sans-serif;
+		margin: 0 auto;
+		position: fixed;
+		z-index: 10;
+		right: 0;
+		left: 0;
+		top: 0;
+		background-color: #fff;
+		border-bottom: 1px solid #ccc;
+		max-width: 1260px;
+		width: 100%;
+		text-align: center;
+		padding: 1em;
+	}
+</style>
