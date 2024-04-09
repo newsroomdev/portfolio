c9ca7b663a29260cd4305482a3c4b65f53e183c6
Gerald Rich <1578563+newsroomdev@users.noreply.github.com>
2024-04-08 18:40:55 -0700

    feat: use pnpm (#52)
    * feat: use pnpm
    
    * fix: pnpm setup
    
    * fix: commit lockfile
    
    * fix: svelte preprocess
    
    * fix: playwright cache
    
    * fix: check cache
    
    * fix: browser mismatch?
    
    * maybe
    
    * use docker img
    
    * fix: playwright ver
    
    * fix: install first
    
    ---------
    
    Co-authored-by: Gerald Rich <newsroomdev@users.noreply.github.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,4 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
-import { devices } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
 	webServer: {
@@ -16,24 +15,7 @@ const config: PlaywrightTestConfig = {
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: 'html',
-	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-	projects: [
-		{
-			name: 'chromium',
-			use: { ...devices['Desktop Chrome'] }
-		},
-
-		{
-			name: 'firefox',
-			use: { ...devices['Desktop Firefox'] }
-		},
-
-		{
-			name: 'webkit',
-			use: { ...devices['Desktop Safari'] }
-		}
-	]
+	reporter: 'html'
 };
 
 export default config;
